ee91dc36b7d110e134719aa021d463c7fb299408
kiissy <kiissy@naver.com>
2020-09-09 19:45:07 +0900

    3. 열쇠 사용하기

--- RoomEscape/RoomEscape.py
+++ RoomEscape/RoomEscape.py
@@ -2,22 +2,35 @@ from bangtal import *
 
 scene1 = Scene('룸1', 'images/배경-1.png') # 장면 생성
 
-closed1 = True
 # 문 이미지 넣고 좌표 삽입
-door1 = Object('images/문-오른쪽-열림.png')
-door1.locate(scene1, 800, 270)
+door1 = Object('images/문-오른쪽-닫힘.png')
+door1.locate(scene1, 800, 270)  # 위치 조절
 door1.show()
+door1.closed = True;
+
+key = Object('images/열쇠.png')
+key.setScale(0.2) # 사이즈 조절
+key.locate(scene1, 600, 150)
+key.show()
 
 # 마우스 클릭 이벤트
 def door1_onMouseAction(x, y, action):
-    global closed1     # 전역임을 알려줘야 함
-    if closed1 == True: # 문이 열린 상태에서 클릭해서 닫히면
-        door1.setImage('images/문-오른쪽-닫힘.png')
-        closed1 = False
+
+    if door1.closed:
+        if key.inHand(): 
+            door1.setImage('images/문-오른쪽-열림.png')
+            door1.closed = False
+        else:
+            showMessage('열쇠가 필요합니다.')
     else:
         endGame()   # 종료 함수
 
 door1.onMouseAction = door1_onMouseAction
+
+def key_onMouseAction(x, y, action):
+    key.pick()
+
+key.onMouseAction = key_onMouseAction
 
 
 startGame(scene1)

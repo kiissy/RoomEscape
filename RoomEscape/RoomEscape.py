e864c22c9518adfa85f54c936bf6d84e8210aba1
kiissy <kiissy@naver.com>
2020-09-10 03:06:24 +0900

    과제 끝

--- RoomEscape/RoomEscape.py
+++ RoomEscape/RoomEscape.py
@@ -81,10 +81,10 @@ key.locate(scene1, 700, 400)
 # 룸2
 scene2 = Scene('연구실', 'images/배경-2.png')
 
-light = Object('images/전등.png')
-light.setScale(0.2)
-light.locate(scene2, 550, 450)
-light.show()
+lamp1 = Object('images/전등.png')
+lamp1.setScale(0.2)
+lamp1.locate(scene2, 550, 450)
+lamp1.show()
 
 door2 = Object('images/문-왼쪽-열림.png')
 door2.locate(scene2, 320, 270)  # 위치 조절
@@ -94,45 +94,47 @@ door3 = Object('images/문-오른쪽-닫힘.png')
 door3.locate(scene2, 900, 270)  # 위치 조절
 door3.show()
 
-#table = Object('images/책상.png')
-#table.setScale(0.4)
-#table.locate(scene2, 460, 50)
-#table.show()
+table = Object('images/책상.png')
+table.setScale(0.4)
+table.locate(scene2, 460, 50)
+table.show()
 
-#ladder = Object('images/사다리.png')
-#ladder.setScale(0.6)
-#ladder.locate(scene2, 1100, 90)
-#ladder.show()
+ladder = Object('images/사다리.png')
+ladder.setScale(0.6)
+ladder.locate(scene2, 1100, 90)
+ladder.show()
 
 keypad = Object('images/키패드.png')
 keypad.locate(scene2, 885, 420)
 keypad.show()
 
-#password = Object('images/암호.png')
-#password.setScale(0.8)
-#password.locate(scene2, 350, 30)
+password = Object('images/암호.png')
+password.setScale(0.7)
+password.locate(scene2, 400, 30)
 #password.show()
 
 # 룸3
 scene3 = Scene('???', 'images/배경-2.png')
 
 door4 = Object('images/문-왼쪽-열림.png')
-door4.locate(scene2, 320, 270)  # 위치 조절
+door4.locate(scene3, 320, 270)  # 위치 조절
 door4.show()
-
-light = Object('images/전등.png')
-light.setScale(0.2)
-light.locate(scene3, 550, 450)
-light.show()
 
 memo3 = Object('images/쪽지.png')
 memo3.setScale(0.15) # 사이즈 조절
 memo3.locate(scene3, 600, 145)
 memo3.show()
 
+endMemo = Object('images/끝메모.png')
+endMemo.setScale(0.3)
+endMemo.locate(scene3, 320, 100)
+#endMemo.show()
 
-# 마우스 클릭 이벤트
 
+################ 마우스 클릭 이벤트 ###################
+
+
+###### 교수실 ########
 # 책장
 def bookshelf_onMouseAction(x, y, action):
     openbook.show()
@@ -144,16 +146,17 @@ openbook.onMouseAction = openbook_onMouseAction
 
 # 금고 위 교수 쪽지
 def safeMemo_onMouseAction(x, y, action):
+    safeMemo.hide()
     proMemo.show()
 safeMemo.onMouseAction = safeMemo_onMouseAction
 
 def proMemo_onMouseAction(x, y, action):
-    proMemo.hide()
+    proMemo.pick()
 proMemo.onMouseAction = proMemo_onMouseAction
 
 # 금고 키패드
 def safepad_onMouseAction(x, y, action):
-    showKeypad('CAB', key)
+    showKeypad('CAU', key)
 safepad.onMouseAction = safepad_onMouseAction
 
 # 맞추면 열쇠 뜨게
@@ -183,11 +186,12 @@ door1.onMouseAction = door1_onMouseAction
 
 # 벽 메모 보이기
 def doorMemo_onMouseAction(x, y, action):
+    doorMemo.hide()
     wallMemo.show()
 doorMemo.onMouseAction = doorMemo_onMouseAction
 
 def wallMemo_onMouseAction(x, y, action):
-    wallMemo.hide()
+    wallMemo.pick()
 wallMemo.onMouseAction = wallMemo_onMouseAction
 
 # 화분
@@ -201,17 +205,72 @@ def flowerpot_onMouseAction(x, y, action):
             flowerpot.locate(scene1, 650, 150)
             flowerpot.moved = True
         else:
-            flowerpot.locate(scene1, 450, 200)
+            flowerpot.locate(scene1, 650, 200)
             flowerpot.moved = True
 flowerpot.onMouseAction = flowerpot_onMouseAction
 
 # 화분 쪽지
+def memo1_onMouseAction(x, y, action):
+    memo1.hide()
+    potMemo.show()
+memo1.onMouseAction = memo1_onMouseAction
+
+def potMemo_onMouseAction(x, y, action):
+    potMemo.pick()
+potMemo.onMouseAction = potMemo_onMouseAction
+
+# 전원버튼
+com.lighted = False
+def comBtn_onMouseAction(x, y, action):
+    if com.lighted:
+        com.setImage('images/컴퓨터.png')
+    else:
+        com.setImage('images/컴퓨터암호.png')
+    com.lighted = not com.lighted
+comBtn.onMouseAction = comBtn_onMouseAction
 
 
+####### 연구실 #########
+# 테이블
+table.moved = False
+def table_onMouseAction(x, y, action):
+    if table.moved == False:
+        if action == MouseAction.DRAG_LEFT:
+            table.locate(scene2, 50, 50)
+            table.moved = True
+table.onMouseAction = table_onMouseAction
+
+# 사다리
+ladder.moved = False
+def ladder_onMouseAction(x, y, action):
+    if table.moved:
+        if ladder.moved == False:
+            if action == MouseAction.DRAG_LEFT:
+                ladder.locate(scene2, 550, 30)
+                ladder.moved = True
+    else:
+        showMessage('물체에 가로막혀 이동할 수 없습니다.')
+ladder.onMouseAction = ladder_onMouseAction
+
+# 전등
+def lamp1_onMouseAction(x, y, action):
+    if ladder.moved:
+        if action == MouseAction.DRAG_DOWN:
+            scene2.setLight(0.2)
+            password.show()
+        elif action == MouseAction.DRAG_UP:
+            scene2.setLight(1)
+            password.hide()
+    else:
+        showMessage('전등이 너무 높습니다.')
+lamp1.onMouseAction = lamp1_onMouseAction
+
+# 문2
 def door2_onMouseAction(x, y, action):
     scene1.enter()
 door2.onMouseAction = door2_onMouseAction
 
+# 문3
 door3.locked = True
 door3.closed = True
 def door3_onMouseAction(x, y, action):
@@ -221,7 +280,7 @@ def door3_onMouseAction(x, y, action):
         door3.setImage('images/문-오른쪽-열림.png')
         door3.closed = False
     else:
-        endGame()   # 종료 함수
+        scene3.enter()
 door3.onMouseAction = door3_onMouseAction
 
 def door3_onKeypad():
@@ -229,19 +288,23 @@ def door3_onKeypad():
     showMessage('문이 열렸습니다.')
 door3.onKeypad = door3_onKeypad
 
+# 키패드
 def keypad_onMouseAction(x, y, action):
-    showKeypad('BANGTAL', door3)
+    showKeypad('ESCAPE', door3)
 keypad.onMouseAction = keypad_onMouseAction
 
-#switch.lighted = True
-#def switch_onMouseAction(x, y, action):
-#    switch.lighted = not switch.lighted
-#    if switch.lighted:
-#        scene2.setLight(1)
-#        password.hide()
-#    else:
-#        scene2.setLight(0.2)
-#        password.show()
-#switch.onMouseAction = switch_onMouseAction
+
+
+
+########## ??? ###########
+def memo3_onMouseAction(x, y, action):
+    memo3.hide()
+    endMemo.show()
+memo3.onMouseAction = memo3_onMouseAction
+
+def endMemo_onMouseAction(x, y, action):
+    endGame()
+endMemo.onMouseAction = endMemo_onMouseAction
+
 
 startGame(scene1)
